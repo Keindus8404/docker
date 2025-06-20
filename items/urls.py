d840c1df636473c8c8bebffd58d6073c3f36ece7
keindus8404 <defrican@gmail.com>
2025-06-20 17:49:58 +0900

    수업 마무리

--- items/urls.py
+++ items/urls.py
@@ -1,26 +1,8 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include
-from views import ItemListView, ItemCreateView, ItemUpdateView, ItemDeleteView
-
+from django.urls import path
+from .views import ItemListView, ItemCreateView,ItemDeleteView,ItemUpdateView
 urlpatterns = [
-    path('', ItemListView.as_view(), name='item_list'),
-    path('create/', ItemCreateView.as_view(), name='item_create'),
-    path('update/<int:pk>/', ItemUpdateView.as_view(), name='item_update'),
-    path('delete/<int:pk>/', ItemDeleteView.as_view(), name='item_delete'),
+    path('', ItemListView.as_view(), name='item-list'),
+    path('add/', ItemCreateView.as_view(), name='item-add'),
+    path('<int:pk>/edit/',ItemUpdateView.as_view(),name='item-edit'),
+    path('<int:pk>/delete/',ItemDeleteView.as_view(),name='item-delete')
 ]
